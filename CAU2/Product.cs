e923a6619c59db2f683e3f498d05267a05e55c68
Nguyen Gia Tuan <nguyengiatuan@Nguyens-MacBook-Pro.local>
2021-07-10 11:44:31 +0700

    Add CAU 2 neeee

---
+++ CAU2/Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace CAU2
+{
+    public class Product
+    {
+        // KHAI BAO CAC THUOC TINH CUA CLASS PRODUCT
+        private string name;
+        private double price;
+        private int number;
+
+        // CONSTRUCTOR KHONG THAM SO
+        public Product()
+        {
+            // Gan tuy y cho doi tuong duoc khoi tao
+            Name = "abc";
+            Price = 100;
+            Number = 1;
+        }
+
+        // CONSTRUCTOR CO THAM SO
+        public Product(string NameProduct, double PriceProduct, int NumberProduct)
+        {
+            // Gan cac gia tri cua tham so cho doi tuong duoc khoi tao
+            Name = NameProduct;
+            Price = PriceProduct;
+            Number = NumberProduct;
+        }
+
+        // KHAI BAO CAC PROPERTIES
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Number { get; set; }
+
+        // Khai bao public de truy xuat
+        public void Input()
+        {
+            // Thuc hien gan gia tri cho product
+            Console.WriteLine("Nhap vao ten san pham: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Nhap vao gia san pham: ");
+            Price = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap vao so luong san pham: ");
+            Number = int.Parse(Console.ReadLine());
+        }
+
+        // Khai bao public de truy xuat
+        public double Pay()
+        {
+            // Khai bao bien tong tien
+            double total = 0;
+            // Tong tien se bang gia tri product nhan voi so luong 
+            total = Number * Price;
+            if(Number >= 4 && Number <=9)
+            {
+                // Neu mua tu 4->9 sp giam 5%
+                return total * 0.95;
+            }
+            else if(Number > 9)
+            {
+                // Neu mua nhieu hon 9 sp giam 10%
+                return total * 0.9;
+            }
+            else
+            {
+                // Neu mua nho hon 4 thi khong giam
+                return total;
+            }
+        }   
+    }
+}
---
+++ CAU2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAU2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // YEU CAU BAI TOAN
+            // CAI DAT LOP PRODUCT DAU TIEN => XEM CLASS PRODUCT
+            // Khai bao danh sach product
+
+            Console.WriteLine("Nhap vao so luong san pham can mua: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> products = new List<Product>(n);
+
+            // Duyet qua tung san pham cho nhap vao thong tin va tinh tong tien
+            // Cach 1:
+            double total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // Khai bao 1 san pham
+                Product p = new Product();
+                // Nhap vao thong tin san pham
+                p.Input();
+                // Tinh tong tien
+                total += p.Pay();
+
+                // Them vao danh sach san pham
+                products.Add(p);
+            }
+
+            Console.WriteLine("Tong tien phai tra la: "+total);
+        }
+    }
+}
